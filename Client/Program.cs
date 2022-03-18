02d5c23b26352429767232dcc39c0d64275f3507
lzabik <lukasz.zabik@gmail.com>
2022-03-18 13:31:49 +0100

    changes test

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,7 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+
 builder.Services.AddStaticWebAppsAuthentication();
 
 await builder.Build().RunAsync();
